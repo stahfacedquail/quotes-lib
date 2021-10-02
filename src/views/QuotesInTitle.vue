23a016b4efba42b3b6f332ccbcf29ff465bcbe5e
Tami Maiwashe <stahfacedquail@gmail.com>
2021-10-02 16:38:31 +0200

    Page for displaying quotes for a particular title.  Boilerplate only.

---
+++ src/views/QuotesInTitle.vue
@@ -0,0 +1,19 @@
+<template>
+    
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { } from "@ionic/vue";
+
+export default defineComponent({
+    name: "QuotesInTitle",
+    components: {
+
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
