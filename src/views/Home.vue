7c9627af7da557528ebc070f9548b837231928f0
Tami Maiwashe <stahfacedquail@gmail.com>
2021-10-02 17:42:35 +0200

    Refresh when loading QuotesInTitle so that new id is picked up

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -38,7 +38,7 @@
           :title="obj.title"
           :authors="obj.authors"
 
-          :router-link="'/quotes/title/' + obj.title.id"
+          :href="'/quotes/title/' + obj.title.id"
         />
       </ion-list>
     </ion-content>
