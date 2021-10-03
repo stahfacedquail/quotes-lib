7091279402ac3a0f25ff1637cc4801ce845f97c1
Tami Maiwashe <stahfacedquail@gmail.com>
2021-10-03 20:41:42 +0200

    Extracted autocomplete field into its own component; use for both title and author search

---
+++ src/components/AutoComplete.vue
@@ -0,0 +1,51 @@
+<template>
+    <ion-item>
+        <ion-label position="stacked">{{ name }}</ion-label>
+        <ion-input v-model="searchValue" @ionChange="updateSearchResults" @ionFocus="updateSearchResults" @ionBlur="searchResults = []" />
+    </ion-item>
+    <ion-list id="results" v-show="searchValue.length > 0">
+        <!--  @mousedown.prevent prevents blur event so that click event can happen first, so that update
+                of title box can happen before input area loses focus -->
+        <ion-item v-for="(result, idx) in searchResults" :key="idx" button @mousedown.prevent @click="updateSearchField(result)">
+            <ion-label>{{ result }}</ion-label>
+        </ion-item>
+    </ion-list>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { IonItem, IonLabel, IonInput, IonList } from "@ionic/vue";
+
+export default defineComponent({
+    name: "AutoComplete",
+    components: {
+        IonItem,
+        IonLabel,
+        IonInput,
+        IonList
+    },
+    props: {
+        name: { type: String, required: true }, //name of the field
+        allValues: { type: Array, required: true } //all possible values (currently) for this field
+    },
+    data() {
+        return {
+            searchValue: "",
+            searchResults: []
+        };
+    }, methods: {
+        updateSearchResults() {
+            this.searchResults = this.allValues.filter(value => value.toLowerCase().includes(this.searchValue.toLowerCase()));
+      
+            /*  Once user chooses a match, Vue will try to search again and create another list with search results
+                This check clears the list if the only result matches what is in the search box */
+            if(this.searchResults.length == 1 && this.searchResults[0] == this.searchValue)
+                this.searchResults = [];
+        },
+        updateSearchField(result) {
+            this.searchValue = result;
+            this.searchResults = []; //clear search results after match selected
+        }
+    }
+})
+</script>
--- src/views/AddQuote.vue
+++ src/views/AddQuote.vue
@@ -9,26 +9,17 @@
             <ion-textarea class="inputQuote" rows="10"></ion-textarea>
           </ion-item>
 
-          <ion-item>
-            <ion-label position="stacked">Title</ion-label>
-            <ion-input v-model="titleSearch" @ionChange="updateTitleList" @ionBlur="done" />
-          </ion-item>
-          <ion-list id="titleResults" v-show="titleSearch.length > 0">
-            <!--  @mousedown.prevent prevents blur event so that click event can happen first, so that update
-                  of title box can happen before input area loses focus -->
-            <ion-item v-for="(result, idx) in titleResults" :key="idx" button @mousedown.prevent @click="updateTitle(result)">
-              <ion-label>{{ result }}</ion-label>
-            </ion-item>
-          </ion-list>
+          <AutoComplete :name="'Title'" :allValues="titles" />
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script>
-import { IonContent, IonPage, IonTextarea, IonLabel, IonItem, IonInput, IonList } from '@ionic/vue';
+import { IonContent, IonPage, IonTextarea, IonLabel, IonItem } from '@ionic/vue';
 import { defineComponent } from 'vue';
 import AppHeader from "../components/AppHeader.vue";
+import AutoComplete from "../components/AutoComplete.vue";
 import db from '../db/mockDb.js';
 
 export default defineComponent({
@@ -40,34 +31,17 @@ export default defineComponent({
     AppHeader,
     IonLabel,
     IonItem,
-    IonInput,
-    IonList
+    AutoComplete
   },
 
   data() {
     return {
       titles: db.getAllTitles(),
-      titleSearch: "",
-      titleResults: []
     };
   },
 
   methods: {
-    updateTitleList() {
-      this.titleResults = this.titles.filter(title => title.toLowerCase().includes(this.titleSearch.toLowerCase()));
-      
-      /*  Once user chooses a match, Vue will try to search again and create another list with search results
-          This check clears the list if the only result matches what is in the search box */
-      if(this.titleResults.length == 1 && this.titleResults[0] == this.titleSearch)
-        this.titleResults = [];
-    },
-    updateTitle(result) {
-      this.titleSearch = result;
-      this.titleResults = []; //clear search results after match selected
-    },
-    done() {
-      this.titleResults = []; //remove search list if we are no longer searching
-    }
+    
   }
 });
 </script>
