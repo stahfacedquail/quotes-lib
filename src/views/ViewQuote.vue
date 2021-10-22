19c429f6cfe2de36088e98bb8c9216f4d3c2b3a9
Tami Maiwashe <stahfacedquail@gmail.com>
2021-10-22 21:11:27 +0200

    Done migrating VIEWQUOTE

--- src/views/ViewQuote.vue
+++ src/views/ViewQuote.vue
@@ -4,23 +4,23 @@
 
     <ion-content :fullscreen="true">    
         <div id="container">
-            <p>{{ data.quote.text }}</p>
+            <p>{{ quote.text }}</p>
             <br>
-            <a v-show="data.title && data.title.url" :href="data.title ? data.title.url : ''" style="text-align:right;font-style:italic;">— {{ data.title ? data.title.value : "" }}</a>
-            <p v-show="data.title && data.title.url == null" style="text-align:right;font-style:italic;">— {{ data.title ? data.title.value : "" }}</p>
+            <a v-show="quote.title && quote.title.url" :href="quote.title ? quote.title.url : ''" style="text-align:right;font-style:italic;">— {{ quote.title ? quote.title.value : "" }}</a>
+            <p v-show="quote.title && quote.title.url == null" style="text-align:right;font-style:italic;">— {{ quote.title ? quote.title.value : "" }}</p>
             
             <br>
             
-            <div v-show="data.authors.length > 0">
+            <div v-show="quote.authors.length > 0">
                 <strong>Authors</strong>
-                <p v-for="(author, idx) in data.authors" :key="idx">{{ author.value }}</p>
+                <p v-for="(author, idx) in quote.authors" :key="idx">{{ author.value }}</p>
             </div>
 
             <br>
 
-            <div v-show="data.tags.length > 0">
+            <div v-show="quote.tags.length > 0">
                 <strong>Tags</strong>
-                <p>{{ data.tags.map(tag => tag.value).join(", ") }}</p>
+                <p>{{ quote.tags.map(tag => tag.value).join(", ") }}</p>
             </div>
         </div>
     </ion-content>
@@ -31,7 +31,6 @@
 import { defineComponent } from 'vue'
 import { IonPage, IonContent } from "@ionic/vue";
 import { useRoute } from "vue-router";
-import db from "../db/mockDb.js";
 import AppHeader from "../components/AppHeader.vue";
 
 export default defineComponent({
@@ -41,15 +40,34 @@ export default defineComponent({
         IonPage,
         IonContent
     },
+
     setup() {
         const route = useRoute();
         const { id } = route.params;
         return { id };
     },
+
     data() {
         return {
-            data: db.getQuoteWithAllAttributes(this.id)
+            quote: {
+                text: "",
+                authors: [],
+                tags: [],
+                title: null
+            }
         };
+    },
+
+    mounted() {
+        return this.getQuoteWithAllAttributes()
+        .then(() => console.log("VIEWQUOTE Done mounting"))
+        .catch(error => console.log("ERROR", error));
+    },
+
+    methods: {
+        getQuoteWithAllAttributes() {
+            return this.$axios.get(`/quote/${this.id}?full=true`).then(({ data }) => this.quote = data);
+        }
     }
 });
 </script>
